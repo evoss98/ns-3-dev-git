3571efc985a7a2cbf77f7a7e1e748e635f1ad527
Ang Li <laimagine@fb.com>
2021-06-01 09:15:35 -0700

    added quantum 25, 560, 1000

--- scratch/plot_figures.py
+++ scratch/plot_figures.py
@@ -114,23 +114,32 @@ print('Saving ' + multiHopThroughputFileName)
 
 
 
+drr25PortToThroughput = {}
 drr50PortToThroughput = {}
 drr100PortToThroughput = {}
 drr200PortToThroughput = {}
 drr500PortToThroughput = {}
+drr560PortToThroughput = {}
+drr1000PortToThroughput = {}
 
+processData('drr_25_receivedPacket.tr', drr25PortToThroughput)
 processData('drr_50_receivedPacket.tr', drr50PortToThroughput)
 processData('drr_100_receivedPacket.tr', drr100PortToThroughput)
 processData('drr_200_receivedPacket.tr', drr200PortToThroughput)
 processData('drr_500_receivedPacket.tr', drr500PortToThroughput)
+processData('drr_560_receivedPacket.tr', drr560PortToThroughput)
+processData('drr_1000_receivedPacket.tr', drr1000PortToThroughput)
 
 drrQuantumFileName = os.path.join(args.dir, 'figures/drr_quantum_throughput.png')
 
 plt.figure()
+plt.plot(drr25PortToThroughput.keys(), drr25PortToThroughput.values(), 'c2-', label='quantum=25')
 plt.plot(drr50PortToThroughput.keys(), drr50PortToThroughput.values(), 'bs-', label='quantum=50')
 plt.plot(drr100PortToThroughput.keys(), drr100PortToThroughput.values(), 'go-', label='quantum=100')
 plt.plot(drr200PortToThroughput.keys(), drr200PortToThroughput.values(), 'r^-', label='quantum=200')
 plt.plot(drr500PortToThroughput.keys(), drr500PortToThroughput.values(), 'y^-', label='quantum=500')
+plt.plot(drr560PortToThroughput.keys(), drr560PortToThroughput.values(), 'k>-', label='quantum=560')
+plt.plot(drr1000PortToThroughput.keys(), drr1000PortToThroughput.values(), 'mx-', label='quantum=1000')
 
 plt.legend(loc='lower right')
 plt.ylabel('Total Throughput Received (Kbits)')
@@ -140,38 +149,41 @@ plt.ylim(900, 1200)
 plt.savefig(drrQuantumFileName)
 print('Saving ' + drrQuantumFileName)
 
-#process delay data. drr_50.xml, fifo_50.xml, sfq_50.xml
 
-files = ["drr_50.xml", "fifo_50.xml", "sfq_50.xml"]
-dictionary_data = {}
 
 print("processing delay data...")
 
-for file_name in files:
-    name = os.path.join(args.dir, file_name)
-    dictionary = {}
+def processDelayFiles(files):
+    dictionary_data = {}
+    for file_name in files:
+        name = os.path.join(args.dir, file_name)
+        dictionary = {}
 
-    print("processing file: " + name)
-    tree = ET.parse(name)
-    root = tree.getroot()
+        print("processing file: " + name)
+        tree = ET.parse(name)
+        root = tree.getroot()
 
-    count = 0
-    for flowStats in root:
-        for flow in flowStats:
-            if 'delaySum' in flow.attrib:
-                count += 1
-                delay_string = flow.attrib['delaySum']
-                num_packets = flow.attrib['rxPackets']
-                ns = delay_string.find("ns")
-                delay_string = delay_string[0: ns]
-                delay = float(delay_string)
-                dictionary[count] = delay / float(num_packets)
+        count = 0
+        for flowStats in root:
+            for flow in flowStats:
+                if 'delaySum' in flow.attrib:
+                    count += 1
+                    delay_string = flow.attrib['delaySum']
+                    num_packets = flow.attrib['rxPackets']
+                    ns = delay_string.find("ns")
+                    delay_string = delay_string[0: ns]
+                    delay = float(delay_string)
+                    dictionary[count] = delay / float(num_packets)
 
-    dictionary_data[file_name] = dictionary
+        dictionary_data[file_name] = dictionary
+
+    return dictionary_data
 
 
 delayFileName = os.path.join(args.dir, 'figures/flows_avg_delay_50.png')
 
+files = ["drr_50.xml", "fifo_50.xml", "sfq_50.xml"]
+dictionary_data = processDelayFiles(files)
 drr50delay = dictionary_data[files[0]]
 fifo50delay = dictionary_data[files[1]]
 sfq50delay = dictionary_data[files[2]]
@@ -186,3 +198,34 @@ plt.xlabel('Flow')
 plt.title('Avg Delay Per Packet using quantum = 50')
 plt.savefig(delayFileName)
 print('Saving ' + delayFileName)
+
+
+print("processing delay data for different quantum values...")
+
+files = ["drr_25.xml", "drr_50.xml", "drr_100.xml", "drr_200.xml", "drr_500.xml", "drr_560.xml", "drr_1000.xml"]
+dictionary_data = processDelayFiles(files)
+
+delayWithQuantumFileName = os.path.join(args.dir, 'figures/flows_delay_quantum_values.png')
+
+drr25delay = dictionary_data[files[0]]
+drr50delay = dictionary_data[files[1]]
+drr100delay = dictionary_data[files[2]]
+drr200delay = dictionary_data[files[3]]
+drr500delay = dictionary_data[files[4]]
+drr560delay = dictionary_data[files[5]]
+drr1000delay = dictionary_data[files[6]]
+
+plt.figure()
+plt.plot(drr25delay.keys(), drr25delay.values(), 'c2-', label='quantum=25')
+plt.plot(drr50delay.keys(), drr50delay.values(), 'bs-', label='quantum=50')
+plt.plot(drr100delay.keys(), drr100delay.values(), 'go-', label='quantum=100')
+plt.plot(drr200delay.keys(), drr200delay.values(), 'r^-', label='quantum=200')
+plt.plot(drr500delay.keys(), drr500delay.values(), 'yo-', label='quantum=500')
+plt.plot(drr560delay.keys(), drr560delay.values(), 'k>-', label='quantum=560')
+plt.plot(drr1000delay.keys(), drr1000delay.values(), 'mx-', label='quantum=1000')
+plt.legend(loc='upper right')
+plt.ylabel('Avg Delay Per Packet (ns)')
+plt.xlabel('Flow')
+plt.title('Avg Delay Per Packet using different quantum values')
+plt.savefig(delayWithQuantumFileName)
+print('Saving ' + delayWithQuantumFileName)
